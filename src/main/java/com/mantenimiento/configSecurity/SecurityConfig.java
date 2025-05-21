c28655f587121601a1a500c278e9c4a768a52ba4
Choza <rafael.choza@envistaco.com>
2025-05-21 16:35:54 -0600

    add spring security and correct CORS configuration

--- src/main/java/com/mantenimiento/configSecurity/SecurityConfig.java
+++ src/main/java/com/mantenimiento/configSecurity/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mantenimiento.configSecurity;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mantenimiento.jwt.JwtAuthenticationFilter;
 
@@ -23,19 +28,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf
-                        .disable())
-                .headers(headers -> headers
-                        .frameOptions(frameOptions -> frameOptions.disable()))
-                .authorizeHttpRequests(authRequest -> authRequest
-                        .requestMatchers("/auth/**", "/h2-console/**").permitAll()
-                        .requestMatchers("/matenimiento/**").authenticated()
-                        .anyRequest().authenticated())
-                .sessionManagement(sessionManager -> sessionManager
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        http.authorizeHttpRequests(authRequest -> authRequest
+                .requestMatchers("/auth/**", "/h2-console/").permitAll()
+                .requestMatchers("/matenimiento/**").authenticated()
+                .anyRequest().authenticated());
+        http.sessionManagement(sessionManager -> sessionManager
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authenticationProvider(authProvider);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
